2345b83e1ed9dd756bf295c25855d753b4b0d204
HMKao1019 <47691175+HMKao1019@users.noreply.github.com>
2022-03-17 13:06:36 +0800

    Add files via upload

--- Part2_Line Developers/bot/urls.py
+++ Part2_Line Developers/bot/urls.py
@@ -8,6 +8,8 @@ Created on Tue Mar 15 14:14:59 2022
 from django.conf.urls import include, url
 from . import views
 
+
+# 用來串接callback主程式
 urlpatterns= [
         url('^callback/', views.callback),
         ]
